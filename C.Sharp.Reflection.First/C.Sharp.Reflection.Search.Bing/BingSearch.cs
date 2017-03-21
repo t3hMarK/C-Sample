4c57b8b86ccc8ebedff9d377fb1584b44e4cb3df
Marc Legault <marclego@gmail.com>
2017-03-21 10:12:47 -0400

    Added search implementations

---
+++ C.Sharp.Reflection.First/C.Sharp.Reflection.Search.Bing/BingSearch.cs
@@ -0,0 +1,7 @@
+﻿namespace C.Sharp.Reflection.Search.Bing
+{
+    public class BingSearch : ISearch
+    {
+        public string Search(string term) => "Bing... seriously...";
+    }
+}
---
+++ C.Sharp.Reflection.First/C.Sharp.Reflection.Search.Google/GoogleSearch.cs
@@ -0,0 +1,11 @@
+﻿namespace C.Sharp.Reflection.Search.Google
+{
+    public class GoogleSearch : ISearch
+    {
+        #region ISearch Implementation
+
+        public string Search(string term) => "Google is down";
+
+        #endregion
+    }
+}
