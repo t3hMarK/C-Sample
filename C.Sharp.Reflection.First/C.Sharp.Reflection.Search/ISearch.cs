3ba4b22f74e3a1d045f22951587b075e2aae6393
Marc Legault <marclego@gmail.com>
2017-03-21 10:03:44 -0400

    Added stub search interface

---
+++ C.Sharp.Reflection.First/C.Sharp.Reflection.Search/ISearch.cs
@@ -0,0 +1,12 @@
+﻿namespace C.Sharp.Reflection.Search
+{
+    public interface ISearch
+    {
+        /// <summary>
+        /// Search for a term
+        /// </summary>
+        /// <param name="term">Term of search</param>
+        /// <returns>Search result</returns>
+        string Search(string term);
+    }
+}
