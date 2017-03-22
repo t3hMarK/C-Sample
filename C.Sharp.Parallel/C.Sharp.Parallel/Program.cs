33e40e9f95ee26d6184df2cf86e193c577f89afd
Marc Legault <marclego@gmail.com>
2017-03-22 08:36:31 -0400

    Removed useless test solution Added new Parallel solution

--- C.Sharp.Parallel/C.Sharp.Parallel/Program.cs
+++ C.Sharp.Parallel/C.Sharp.Parallel/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C.Sharp.Test
+namespace C.Sharp.Parallel
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world from C#");
         }
     }
 }
--- C.Sharp.Parallel/C.Sharp.Parallel/Properties/AssemblyInfo.cs
+++ C.Sharp.Parallel/C.Sharp.Parallel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("C.Sharp.Test")]
+[assembly: AssemblyTitle("C.Sharp.Parallel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("C.Sharp.Test")]
+[assembly: AssemblyProduct("C.Sharp.Parallel")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("f052a612-93cb-4b41-a338-8a6dd79d50d0")]
+[assembly: Guid("04b7f12b-181e-406d-a7b1-4bca810aa1e5")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
